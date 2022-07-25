97635095639317d7daa2690f2f06997dfdea0173
Tahir <Tahirhussain32145@gmail.com>
2022-07-25 08:34:31 +0545

    button part 1

---
+++ src/components/MuiButton.tsx
@@ -0,0 +1,38 @@
+import { Stack,Button, IconButton } from "@mui/material"
+import SendIcon from '@mui/icons-material/Send';
+
+export const MuiButton = () => {
+  return (
+    <Stack spacing={2}>
+    <Stack spacing={2} direction='row'>
+        <Button variant="text" href="https://google.com">Text</Button>
+        <Button variant="contained">Text</Button>
+        <Button variant="outlined">Text</Button>
+
+    </Stack>
+     
+     <Stack spacing={2} direction='row'>
+        <Button variant="contained" color="primary" >Primary</Button>
+        <Button variant="text" color="secondary" >secondary</Button>
+        <Button variant="contained" color="error" >error</Button>
+        <Button variant="outlined" color="warning" >warning</Button>
+        <Button variant="contained" color="success" >success</Button>
+
+     </Stack>
+
+     <Stack display='block' spacing={2} direction='row'>
+        <Button variant='contained' size='small'>Small</Button>
+        <Button variant='contained' size='medium'>medium</Button>
+        <Button variant='contained' size='large'>large</Button>
+
+     </Stack>
+
+     <Stack spacing={2} direction='row'>
+        <Button variant='contained' startIcon={<SendIcon/>} disableRipple onClick={() => alert('hello')}>Send</Button>
+        <Button variant='contained' endIcon={<SendIcon/>} disableElevation>Send</Button>
+      <IconButton aria-label="send" color='error' size='small'><SendIcon/></IconButton>
+     </Stack>
+
+    </Stack>
+  )
+}
