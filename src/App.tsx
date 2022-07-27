28cee6369b63c5636d89abf3ed302782e22387fa
Tahir <Tahirhussain32145@gmail.com>
2022-07-27 14:22:21 +0545

    Select Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 
 import './App.css';
-import { MuiButton } from './components/MuiButton';
-import { MuiTextField } from './components/MuiTextField';
+// import { MuiButton } from './components/MuiButton';
+import { MuiSelect } from './components/MuiSelect';
+// import { MuiTextField } from './components/MuiTextField';
 // import { MuiTypography } from './components/MuiTypography';
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
     <div className="App">
       {/* <MuiTypography/> */}
       {/* <MuiButton/> */}
-      <MuiTextField/>
+      <MuiSelect/>
+      {/* <MuiTextField/> */}
 
     </div>
   );
